2cb48e282c53d25081388946193e3acf77306b87
Nebo <kindersmail08@gmail.com>
2022-09-16 04:05:24 +0300

    start test service

--- Questions/Presentation/Controllers/Main/QMainViewModel.swift
+++ Questions/Presentation/Controllers/Main/QMainViewModel.swift
@@ -6,7 +6,25 @@
 //
 
 import Foundation
+import RxSwift
+import RxRelay
 
 class QMainViewModel: QBaseViewModel {
+    
+    let tmpService: TmpServiceProtocol
+    let rxString = BehaviorRelay(value: "")
+    let dispBag = DisposeBag()
+    
+    init(serviceFactory: QServiceFactoryProtocol) {
+        tmpService = serviceFactory.ProvideTmpService()
+        super.init()
+        
+        _ = tmpService.rxTmpFunc().subscribe(onSuccess: { rr in
+            print(rr)
+        }, onFailure: { error in
+            print(error)
+        }).disposed(by: dispBag)
+    }
+    
     
 }
